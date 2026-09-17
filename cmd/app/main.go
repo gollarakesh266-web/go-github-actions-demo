473d1609d6bf211451c4e79fc9443762edfc91c1
Rakesh <gollarakesh2306@gmail.com>
2026-09-17 06:49:36 +0000

    Initial Go microservice

---
+++ cmd/app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "OK")
+}
+
+func main() {
+	http.HandleFunc("/health", healthHandler)
+
+	fmt.Println("Server running on port 8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gollarakesh266-web/go-github-actions-demo
+
+go 1.24.6
